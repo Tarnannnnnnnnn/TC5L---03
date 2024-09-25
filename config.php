76d9084f77d05cef0f4185cd793420eddee5cb2c
Vengka03 <165362890+Vengka03@users.noreply.github.com>
2024-09-25 21:01:10 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root"; // default XAMPP MySQL username
 $password = ""; // default XAMPP MySQL password is blank
-$dbname = "recipe_database";
+$dbname = "recipes_database";
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
